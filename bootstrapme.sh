bb0ee35ab13b5c2ce7e937ee25701a08d52a368f
Surojit Pathak <surojit.pathak@gmail.com>
2015-11-30 23:09:47 +0000

    For RHEL6.x pip-installation takes few more steps

--- bootstrapme.sh
+++ bootstrapme.sh
@@ -5,7 +5,14 @@ sudo yum install -y gcc
 sudo yum install -y wget
 sudo yum install -y git
 sudo yum install -y cscope
+
 sudo easy_install pip
+if [ $? -ne 0 ]; then
+    sudo rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    sudo yum install -y python-pip
+    sudo rpm -e epel-release
+fi
+
 
 # gets suro_git_clone
 source ~/github-surojit-pathak/rcS/bashrc
